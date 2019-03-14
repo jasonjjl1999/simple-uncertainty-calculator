536583d8044ee495fd585587b96e0b2b4d4ab84a
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-03-14 17:44:32 -0400

    Formatting

--- chiSquared.py
+++ chiSquared.py
@@ -1,3 +1,5 @@
 def chi(fxvals, yvals, yerr):
-    chiSquared = sum(map(lambda fx, y, e: ((y - fx)**2)/e**2, fxvals, yvals, yerr))
+    chiSquared = sum(
+        map(lambda fx, y, e: ((y - fx) ** 2) / e ** 2, fxvals, yvals, yerr)
+    )
     return chiSquared
